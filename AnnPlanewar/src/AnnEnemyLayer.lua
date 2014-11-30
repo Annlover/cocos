50fcad7bdfa770ea77ce3532b91dff100e32cf01
张寰 <m13818239270@163.com>
2014-11-30 19:10:17 +0800

    新增撞击事件处理

--- AnnPlanewar/src/AnnEnemyLayer.lua
+++ AnnPlanewar/src/AnnEnemyLayer.lua
@@ -28,28 +28,53 @@ function AnnEnemyLayer:init()
         local newEnemy = self:getEnemy()
         self:addChild(newEnemy)
         table.insert(enemys,table.maxn(enemys) + 1,newEnemy)
-        for key, var in pairs(enemys) do 
-            local enemy = enemys[key]
-            if enemy:getPositionY() < 0 then
-                enemy:removeFromParent()
-                table.remove(enemys,key)
-            end
-        end
+        
+        local myPlaneX,myPlaneY = planeLayer:getPosition()
+        local shootItem = cc.Sprite:create("single_bullet.png")
+        shootItem:setPosition(newEnemy:getPosition())
+        local roateAction = cc.RotateBy:create(4,3600.0)
+        local shootAction = cc.MoveTo:create(3,cc.p(myPlaneX,-myPlaneY))
+        shootItem:runAction(cc.Spawn:create(shootAction,roateAction))
+        self:addChild(shootItem)
+        table.insert(enemyBullets,table.maxn(enemyBullets) + 1,shootItem)
     end
     
     local function refresh(dt)
-        for key, var in pairs(enemys) do 
-            local enemy = enemys[key]
+        for enemyKey, var in pairs(enemys) do 
+            local enemy = enemys[enemyKey]
             if enemy:getPositionY() < 0 then
                 enemy:removeFromParent()
-                table.remove(enemys,key)
+                table.remove(enemys,enemyKey)
+            else
+                local myPlane_width = planeLayer:getContentSize().width
+                local myPlane_height = planeLayer:getContentSize().height
+                local myPlane_rect_x = planeLayer:getPositionX() - myPlane_width / 2
+                local myPlane_rect_y = planeLayer:getPositionY() - myPlane_height / 2
+                local myPlaneRect = cc.rect(myPlane_rect_x, myPlane_rect_y, myPlane_width, myPlane_height)
+
+                local enemy = enemys[enemyKey]
+                local enemy_width = enemy:getContentSize().width
+                local enemy_height = enemy:getContentSize().height
+                local enemy_rect_x = enemy:getPositionX() - enemy_width / 2
+                local enemy_rect_y = enemy:getPositionY() - enemy_height / 2
+                local enemyRect = cc.rect(enemy_rect_x, enemy_rect_y, enemy_width, enemy_height)
+
+                if cc.rectIntersectsRect(myPlaneRect,enemyRect) then
+                    print("撞击")
+                    enemy:removeFromParent()
+                    table.remove(enemys,enemyKey)
+                    local bomb = require("SpriteBomb"):create(3)
+                    bomb:setPosition(0,0)
+                    planeLayer:addChild(bomb)
+                    break
+                end
             end
         end
     end
 
     local scheduler = cc.Director:getInstance():getScheduler()
     local schedulerID = scheduler:scheduleScriptFunc(update, 1, false)
---    local refreshID = self:scheduleUpdateWithPriorityLua(refresh,0)
+    local refreshID = self:scheduleUpdateWithPriorityLua(refresh,0)
 
 --    cc.Director:getInstance():getScheduler():unscheduleScriptEntry(schedulerID) 
 --    self:scheduleUpdateWithPriorityLua(update, 0)
