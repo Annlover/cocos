c32ff0c4910dda44fdb0951d363536103aee1d3e
张寰 <m13818239270@163.com>
2014-11-30 19:12:28 +0800

    1.新增分数控制 2.新增距离控制 3.新增星星数获得

--- AnnPlanewar/src/AnnItemLayer.lua
+++ AnnPlanewar/src/AnnItemLayer.lua
@@ -11,15 +11,23 @@ function AnnItemLayer:create()
 end
 
 items = {}
+score = 0
+distance = 0
+stars = 0
 
 function AnnItemLayer:init() 
     
     local winSize = cc.Director:getInstance():getWinSize()
 
     local function refresh(dt)
+        score = score + 1
+        scoreLabel:setString(score)
+        distance = distance + 1
+        distanceLabel:setString(distance)
+        starLabel:setString(stars)
         for itemKey, itemVar in pairs(items) do 
             local item = items[itemKey]
-            if item:getPositionY() > winSize.height or item:getPositionX() < 0 or item:getPositionX() > winSize.width then
+            if item:getPositionY() < 0 or item:getPositionX() < 0 or item:getPositionX() > winSize.width then
                 item:removeFromParent()
                 table.remove(items,itemKey)
             else
@@ -35,9 +43,10 @@ function AnnItemLayer:init()
                 local item_rect_y = item:getPositionY() - item_height / 2
                 local itemRect = cc.rect(item_rect_x, item_rect_y, item_width, item_height)
                 if cc.rectIntersectsRect(planeRect,itemRect) then
-                    print("eat star")
+                    score = score + 100
                     item:removeFromParent()
                     table.remove(items,itemKey)
+                    stars = stars + 1
                     break
                 end
             end
@@ -46,6 +55,41 @@ function AnnItemLayer:init()
 
     local scheduler = cc.Director:getInstance():getScheduler()
     local refreshID = self:scheduleUpdateWithPriorityLua(refresh,1)
+    
+    local hpLabel = cc.Sprite:createWithSpriteFrame(cc.SpriteFrameCache:getInstance():getSpriteFrame("ui_011.png"))
+    hpLabel:setAnchorPoint(0,0)
+    hpLabel:setPosition(0,winSize.height - 40)
+    self:addChild(hpLabel, 10)
+    
+    scoreLabel = cc.LabelAtlas:_create(score, "fonts/img_num_score.plist")
+    scoreLabel:setScale(0.6)
+    scoreLabel:setAnchorPoint(1,0)
+    scoreLabel:setPosition(winSize.width - 40,winSize.height - 80)
+    self:addChild(scoreLabel, 0)
+    
+    local starIconLabel = cc.Sprite:createWithSpriteFrame(cc.SpriteFrameCache:getInstance():getSpriteFrame("ui_010.png"))
+    starIconLabel:setPosition(winSize.width - 160,winSize.height - 100)
+    starIconLabel:setScale(0.5)
+    self:addChild(starIconLabel, 10)
+    
+    starLabel = cc.LabelAtlas:_create(stars, "fonts/img_num_star.plist")
+    starLabel:setAnchorPoint(0,0.5)
+    starLabel:setPosition(winSize.width - 130,winSize.height - 115)
+    self:addChild(starLabel, 0)
+    
+    local kLabel = cc.Sprite:createWithSpriteFrame(cc.SpriteFrameCache:getInstance():getSpriteFrame("ui_012.png"))
+    kLabel:setAnchorPoint(1,0)
+    kLabel:setPosition(winSize.width - 20,winSize.height - 180)
+    self:addChild(kLabel, 10)
+    local mLabel = cc.Sprite:createWithSpriteFrame(cc.SpriteFrameCache:getInstance():getSpriteFrame("ui_013.png"))
+    mLabel:setAnchorPoint(1,0)
+    mLabel:setPosition(winSize.width,winSize.height - 180)
+    self:addChild(mLabel, 10)
+    
+    distanceLabel = cc.LabelAtlas:_create(distance, "fonts/img_num_dis.plist")
+    distanceLabel:setAnchorPoint(1,0)
+    distanceLabel:setPosition(winSize.width - 55,winSize.height - 185)
+    self:addChild(distanceLabel, 0)
     
     return true
 end
